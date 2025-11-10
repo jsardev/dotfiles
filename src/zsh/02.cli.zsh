b3fd186eb2e6a91c31b1b74d0aca0a57114dbf2c
Jakub Sarnowski <hello@jsarnowski.com>
2025-11-10 11:05:16 +0100

    chore: add bun shell completions

--- src/zsh/02.cli.zsh
+++ src/zsh/02.cli.zsh
@@ -1,19 +1,22 @@
-# Z
+# z
 . $(brew --prefix)/etc/profile.d/z.sh
 
-# AWS Amplify
+# aws amplify
 export PATH="$HOME/.amplify/bin:$PATH"
 
-# Serverless
+# serverless
 export PATH="$HOME/.serverless/bin:$PATH"
 
-# NVM
+# nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# Atuin
+# atuin
 eval "$(atuin init zsh --disable-up-arrow)"
 
 # fzf
-source <(fzf --zsh)
+FZF_CTRL_R_COMMAND="" source <(fzf --zsh)
+
+# bun
+[ -s "/Users/jsarnowski/.bun/_bun" ] && source "/Users/jsarnowski/.bun/_bun"
