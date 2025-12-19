c67bf30193b79ca251e39c3aa7f659eab19180ea
Jakub Sarnowski <hello@jsarnowski.com>
2025-12-19 09:30:17 +0100

    chore: add claude and opencode configs

--- src/secrets.zsh
+++ src/secrets.zsh
@@ -1,3 +1,4 @@
 export MCP_CONTEXT7_API_KEY="op://Personal/context7 mcp api key/password"
 export MCP_ZAI_API_KEY="op://Personal/i63jq4seqb7j22de3vapfbaolu/password"
+export ANTHROPIC_AUTH_TOKEN="op://Personal/i63jq4seqb7j22de3vapfbaolu/password"
 export RAINDROP_ACCESS_TOKEN="op://Personal/raindrop api token/password"
